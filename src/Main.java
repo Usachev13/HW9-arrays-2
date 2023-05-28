0d7a4c8388e228a1cbb56ffdd7ae17b2bec76dff
Usachev13 <2893318@gmail.com>
2023-05-28 20:15:56 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("HW9 arrays2 starts!");
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -17,9 +18,26 @@ public class Main {
     public static void task1() {
         int[] arr = generateRandomArray();
         int total = 0;
-       for (final int current : arr){
+       for (int current : arr){
            total += current;
        }
         System.out.println("Сумма трат за месяц составит: " + total);
+    }
+
+    /* task2 */
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int minAmount = 200_001;
+        int maxAmount = -1;
+        for (int current : arr){
+            if (current > maxAmount){
+                maxAmount = current;
+            } else if (current < minAmount) {
+                minAmount = current;
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxAmount + " рублей");
+        System.out.println("Минимальная сумма трат за день составила " + minAmount + " рублей");
+
     }
 }
